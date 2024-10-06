d751d63fdb0e60c331a4575d50b3189d126422bf
cristiano <cristianorbfilho@gmail.com>
2024-10-05 23:20:28 -0300

    feat: security group modules and implementation on main.tf

--- main.tf
+++ main.tf
@@ -38,3 +38,98 @@ module "subnet_public_1d" {
   general_tags = local.common_tags
 }
 
+module "public_instance_security_group" {
+  source                     = "./modules/connectivity/security-group"
+  security_group_name        = "${local.common_tags.Project}-${local.common_tags.Pair}-public-instance-sg"
+  security_group_description = "Allows traffic on port 80 came from internet"
+  vpc_id                     = module.vpc_main.vpc_id
+  security_group_egress_rules = [
+    {
+      port            = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  ]
+  security_group_ingress_rules = [
+    {
+      port            = 80
+      protocol        = "TCP"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  ]
+  general_tags = local.common_tags
+}
+
+module "private_instance_security_group" {
+  source                     = "./modules/connectivity/security-group"
+  security_group_name        = "${local.common_tags.Project}-${local.common_tags.Pair}-private-instance-sg"
+  security_group_description = "Allows traffic on port 80 came from public instance security group"
+  vpc_id                     = module.vpc_main.vpc_id
+  security_group_egress_rules = [
+    {
+      port            = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  ]
+  security_group_ingress_rules = [
+    {
+      port            = 80
+      protocol        = "TCP"
+      cidr_blocks     = []
+      security_groups = [module.public_instance_security_group.security_group_id]
+    }
+  ]
+  general_tags = local.common_tags
+}
+
+module "rds_security_group" {
+  source                     = "./modules/connectivity/security-group"
+  security_group_name        = "${local.common_tags.Project}-${local.common_tags.Pair}-sg-private-instance"
+  security_group_description = "Allows traffic on port 3306 came from private instance security group"
+  vpc_id                     = module.vpc_main.vpc_id
+  security_group_egress_rules = [
+    {
+      port            = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  ]
+  security_group_ingress_rules = [
+    {
+      port            = 3306
+      protocol        = "TCP"
+      cidr_blocks     = []
+      security_groups = [module.private_instance_security_group.security_group_id]
+    }
+  ]
+  general_tags = local.common_tags
+}
+
+module "alb_security_group" {
+  source                     = "./modules/connectivity/security-group"
+  security_group_name        = "${local.common_tags.Project}-${local.common_tags.Pair}-application-load-balancer-sg"
+  security_group_description = "Allows traffic on port 80 came from internet"
+  vpc_id                     = module.vpc_main.vpc_id
+  security_group_egress_rules = [
+    {
+      port            = 0
+      protocol        = "-1"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  ]
+  security_group_ingress_rules = [
+    {
+      port            = 80
+      protocol        = "TCP"
+      cidr_blocks     = ["0.0.0.0/0"]
+      security_groups = []
+    }
+  ]
+  general_tags = local.common_tags
+}
