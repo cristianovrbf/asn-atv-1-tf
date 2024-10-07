a5a72e5a946c6f19d317eac06b5ed03b6d73d02a
cristiano <cristianorbfilho@gmail.com>
2024-10-07 09:36:03 -0300

    fix: adjusting setup of the application load balancer

--- main.tf
+++ main.tf
@@ -38,6 +38,17 @@ module "subnet_public_1d" {
   general_tags = local.common_tags
 }
 
+module "subnet_public_1b" {
+  source                   = "./modules/connectivity/subnet"
+  vpc_id                   = module.vpc_main.vpc_id
+  subnet_cidr_block        = cidrsubnet(module.vpc_main.vpc_cidr_block, 8, 4)
+  subnet_availability_zone = "${local.region}b"
+  is_public                = true
+
+  subnet_name  = "${local.common_tags.Project}-${local.common_tags.Pair}-public-subnet-1b"
+  general_tags = local.common_tags
+}
+
 module "public_instance_security_group" {
   source                     = "./modules/connectivity/security-group"
   security_group_name        = "${local.common_tags.Project}-${local.common_tags.Pair}-public-instance-sg"
@@ -88,7 +99,7 @@ module "private_instance_security_group" {
 
 module "rds_security_group" {
   source                     = "./modules/connectivity/security-group"
-  security_group_name        = "${local.common_tags.Project}-${local.common_tags.Pair}-sg-private-instance"
+  security_group_name        = "${local.common_tags.Project}-${local.common_tags.Pair}-rds-private-instance"
   security_group_description = "Allows traffic on port 3306 came from private instance security group"
   vpc_id                     = module.vpc_main.vpc_id
   security_group_egress_rules = [
@@ -168,6 +179,12 @@ module "subnet_public_1d_route_table_association" {
   route_table_id = module.vpc_route_tables.public_route_table_id
 }
 
+module "subnet_public_1b_route_table_association" {
+  source         = "./modules/connectivity/route-table-association"
+  subnet_id      = module.subnet_public_1b.subnet_main_id
+  route_table_id = module.vpc_route_tables.public_route_table_id
+}
+
 module "launch_template" {
   source                        = "./modules/backend/launch-template"
   launch_template_name          = "${local.common_tags.Project}-${local.common_tags.Pair}-apache-launch-template"
@@ -181,11 +198,21 @@ module "launch_template" {
 
 module "auto_scalling_group" {
   source             = "./modules/backend/auto-scalling-group"
-  subnets            = [module.subnet_private_1b.subnet_main_id]
+  subnets            = [module.subnet_public_1d.subnet_main_id]
   launch_template_id = module.launch_template.launch_template_id
   general_tags       = local.common_tags
   asg_name           = "${local.common_tags.Project}-${local.common_tags.Pair}-auto-scalling-group"
   desired_capacity   = 1
   max_size           = 2
   min_size           = 1
+}
+
+module "application_load_balancer" {
+  source                    = "./modules/backend/application-load-balancer"
+  alb_name                  = "${local.common_tags.Project}-${local.common_tags.Pair}-alb"
+  alb_sg_id                 = module.public_instance_security_group.security_group_id
+  subnets                   = [module.subnet_public_1d.subnet_main_id, module.subnet_public_1b.subnet_main_id]
+  alb_main_targe_group_name = "${local.common_tags.Project}-${local.common_tags.Pair}-tg"
+  vpc_id                    = module.vpc_main.vpc_id
+  autoscaling_group_name    = module.auto_scalling_group.auto_scalling_group_name
 }
--- modules/backend/auto-scalling-group/output.tf
+++ modules/backend/auto-scalling-group/output.tf
@@ -5,3 +5,7 @@ output "auto_scalling_group_id" {
 output "auto_scalling_group_arn" {
   value = aws_autoscaling_group.asg.arn
 }
+
+output "auto_scalling_group_name" {
+  value = aws_autoscaling_group.asg.name
+}
